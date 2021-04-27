5bd436195a4e22e911e656d014f63dfc6999bd9e
Matt Koski <mjkekai@gmail.com>
2021-04-26 21:34:04 -0700

    add styling to main page and some js

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var startButton = document.getElementById("startButton")
+var gameContainer = document.getElementById("gameContainer")
+var questions = document.getElementById("questions")
+var answerButton = document.getElementById("answers")
+var highScore
